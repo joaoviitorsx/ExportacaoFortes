01576d22ada88b6ec3d3a461508b0a6e5eba4666
João Vitor Silva <joaovitorssilva007@gmail.com>
2025-09-29 11:12:06 -0300

    ajustado e visualmente melhorado

--- front/src/components/progressBar.py
+++ front/src/components/progressBar.py
@@ -8,34 +8,25 @@ class ProgressBar(ft.Column):
             expand=True,
         )
 
-        self.bar = ft.ProgressBar(width=400, height=12, value=0, bgcolor="#ddd")
+        self.bar = ft.ProgressBar(width=520, height=12, value=0, bgcolor="#ddd", border_radius=6)
         self.status = ft.Text("Aguardando processamento...", size=14, color="grey")
 
-        self.steps = ft.Column(
-            spacing=4,
-            horizontal_alignment=ft.CrossAxisAlignment.START,
-        )
-
-        self.controls = [self.bar, self.status, self.steps]
+        self.controls = [
+            self.bar,
+            self.status
+        ]
 
     def start(self):
         self.bar.value = 0
         self.status.value = "Iniciando..."
-        self.steps.controls.clear()
         self.update()
 
     def set_progress(self, percent: int, message: str):
         self.bar.value = percent / 100
         self.status.value = message
-        self.steps.controls.append(
-            ft.Text(f"✔ {message}", size=12, color=ft.Colors.GREY_800)
-        )
         self.update()
 
     def finish(self):
         self.bar.value = 1
         self.status.value = "Concluído!"
-        self.steps.controls.append(
-            ft.Text("✔ Processamento finalizado com sucesso!", size=12, color=ft.Colors.GREEN_600)
-        )
         self.update()
