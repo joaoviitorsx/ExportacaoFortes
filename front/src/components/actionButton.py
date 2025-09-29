221d77c1024b7b9cd75efba3a15809101f9b93be
João Vitor Silva <joaovitorssilva007@gmail.com>
2025-09-29 11:11:07 -0300

    botao ajustado e manipulavem para o processamento

--- front/src/components/actionButton.py
+++ front/src/components/actionButton.py
@@ -1,30 +1,32 @@
 import flet as ft
 
 class ActionButton(ft.ElevatedButton):
-    def __init__(
-        self,
-        text: str,
-        icon: str | None = None,
-        disabled: bool = False,
-        visible: bool = True,
-        color: str = "primary",
-    ):
-        colors = {
-            "primary": ft.Colors.BLUE_600,
-            "success": ft.Colors.GREEN_600,
-            "danger": ft.Colors.RED_600,
-            "secondary": ft.Colors.GREY_700,
-        }
-
+    def __init__(self, text, icon=None, disabled=False, visible=True, color="primary", on_click=None):
         super().__init__(
             text=text,
             icon=icon,
             disabled=disabled,
             visible=visible,
+            on_click=on_click,
             style=ft.ButtonStyle(
-                shape={"": ft.RoundedRectangleBorder(radius=8)},
-                padding={"": 20},
-                bgcolor={"": colors.get(color, ft.Colors.BLUE_600)},
-                color={"": ft.Colors.WHITE},
+                bgcolor={
+                    ft.ControlState.DEFAULT: self.getBg(color),
+                    ft.ControlState.DISABLED: ft.Colors.GREY_400,
+                },
+                color={
+                    ft.ControlState.DEFAULT: ft.Colors.WHITE,
+                    ft.ControlState.DISABLED: ft.Colors.BLACK87,
+                },
+                padding=ft.padding.symmetric(horizontal=18, vertical=18),
+                shape=ft.RoundedRectangleBorder(radius=8),
             ),
         )
+
+    def getBg(self, color: str):
+        cores = {
+            "primary": ft.Colors.BLUE_600,
+            "success": ft.Colors.GREEN_600,
+            "secondary": ft.Colors.GREY_600,
+            "danger": ft.Colors.RED_600,
+        }
+        return cores.get(color, ft.Colors.BLUE_600)
