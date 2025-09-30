3a25ef9d5c4038609253ce3388e2f9ee679b9253
João Vitor Silva <joaovitorssilva007@gmail.com>
2025-09-30 11:34:18 -0300

    empresa controller para a integração do back e front

--- back/src/controllers/empresaController.py
+++ back/src/controllers/empresaController.py
@@ -1,13 +1,17 @@
 from ..config.db.conexaoFS import getSessionFS
 from ..repositories.empresaRepo.empresaRepository import EmpresaRepository
+from ..services.cnpjRegister.cnpjService import CnpjService
 
 class EmpresaController:
     def __init__(self):
         self.session = getSessionFS()
         self.repo = EmpresaRepository(self.session)
 
-    def listar_empresas(self):
+    def listarEmpresas(self):
         return self.repo.get_all()
 
-    def cadastrar_empresa(self, razao_social: str, cnpj: str):
-        return self.repo.insert(razao_social, cnpj)
+    def cadastrarEmpresas(self, razao_social: str, cnpj: str, uf: str, simples: bool):
+        return self.repo.insert(razao_social, cnpj, uf, simples)
+    
+    def buscarCnpj(self, cnpj: str):
+        return CnpjService.consultarCnpj(cnpj)
