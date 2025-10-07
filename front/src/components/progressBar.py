3bd2dadb2f160cad28fafa6c1081bb4e23024cd9
João Vitor Silva <joaovitorssilva007@gmail.com>
2025-10-07 14:48:49 -0300

    ajuste na class do download progress bar para ficar ideterminate

--- front/src/components/progressBar.py
+++ front/src/components/progressBar.py
@@ -34,13 +34,13 @@ class ProgressBar(ft.Column):
 class DownloadProgressBar(ft.Column):
     def __init__(self):
         super().__init__(
-            spacing=10,
+            spacing=15,
             horizontal_alignment=ft.CrossAxisAlignment.CENTER,
             expand=True,
         )
 
-        self.bar = ft.ProgressBar(width=620, height=12, value=0, bgcolor="#ddd", border_radius=6)
-        self.status = ft.Text("Preparando download...", size=14, color="grey")
+        self.bar = ft.ProgressBar(width=620, height=12, color=ft.Colors.BLUE_600, bgcolor="#ddd", border_radius=6)
+        self.status = ft.Text("Gerando arquivo...", size=14, color="grey")
 
         self.controls = [
             self.bar,
@@ -48,16 +48,17 @@ class DownloadProgressBar(ft.Column):
         ]
 
     def start(self):
-        self.bar.value = 0
-        self.status.value = "Gerando o arquivo..."
-        self.update()
-
-    def set_progress(self, percent: int, message: str):
-        self.bar.value = percent / 100
-        self.status.value = message
+        self.bar.visible = True
+        self.status.value = "Gerando arquivo..."
         self.update()
 
     def finish(self):
-        self.bar.value = 1
+        self.bar.visible = False
         self.status.value = "Arquivo gerado com sucesso!"
+        self.status.color = ft.Colors.GREEN_600
+        self.update()
+
+    def hide(self):
+        self.bar.visible = False
+        self.status.value = ""
         self.update()
