961896210ca6903d1163713a94e5bec91556c0a0
João Vitor Silva <joaovitorssilva007@gmail.com>
2025-10-07 09:26:09 -0300

    registro CAB

--- back/src/services/fs/CAB/builderCAB.py
+++ back/src/services/fs/CAB/builderCAB.py
@@ -5,15 +5,15 @@ from ....utils.fsFormat import formatarData
 def builderCAB(empresa: Dict[str, Any], registro_0000: Dict[str, Any]) -> str:
     tipo_registro = "CAB"
     versao_leiaute = "172"
-    software = "RSFiscal"[:62]
+    software = "AssertivusFiscal"[:62]
     data_geracao = datetime.now().strftime("%Y%m%d")
     nome_empresa = str(empresa.get("razao_social", ''))[:15]
     data_inicial = formatarData(registro_0000.get("dt_ini")) 
     data_final = formatarData(registro_0000.get("dt_fin"))
     periodo_formatado = str(registro_0000.get('periodo', '')).replace('/', '')
     descricao = f"ENTRADAS {nome_empresa} {periodo_formatado}"[:40] 
-    aliquotas_especificas_flag = empresa.get("aliq_espec", False)
-    flag_aliq_especificas = "S" if aliquotas_especificas_flag else "N"
+    #aliquotas_especificas_flag = empresa.get("aliq_espec", False)
+    flag_aliq_especificas = "S"
 
     linha_cab = "|".join([
         tipo_registro,
@@ -27,4 +27,4 @@ def builderCAB(empresa: Dict[str, Any], registro_0000: Dict[str, Any]) -> str:
         flag_aliq_especificas
     ])
     
-    return linha_cab
+    return linha_cab + "|"
